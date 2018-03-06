1fd35b91862b0c224753d86869c50fba7e5ca64f
Sethu Madhavan <sethu969@gmail.com>
2018-03-06 13:43:56 -0500

    Init project,added fonts,colors to theme file

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FinancesPage } from '../finances/finances';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  navToFinance() {
+  	this.navCtrl.push(FinancesPage);
   }
 
 }
